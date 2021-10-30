09786e32c8cce64774fcbbe8948d6e5fb222392d
Netyloft <netesov.9882@gmail.com>
2021-10-30 15:35:53 +0500

    Ешё

---
+++ PasswordWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace AnimalRegister
+{
+    public partial class PasswordWindow : Window
+    {
+        public PasswordWindow()
+        {
+            InitializeComponent();
+            
+            Button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            Close();
+            mainWindow.Show();
+        }
+    }
+}
