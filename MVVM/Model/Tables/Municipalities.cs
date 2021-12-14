79311f5f8f1808ab8e602a62d25fcc4f6c391692
Ivan Snowe <barabum999@gmail.com>
2021-12-14 15:11:00 +0500

    add tables

---
+++ MVVM/Model/Tables/Municipalities.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AnimalRegister.MVVM.Model.Tables
+{
+    class Municipalities
+    {
+        public int Id;
+        public string Name;
+        public string LocalGoverment; 
+    }
+}
