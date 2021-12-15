72e6a09d6642057f6b88159bdeb7a3e60dc0991f
Snowe <barabum999@gmail.com>
2021-12-16 00:18:47 +0500

    added log repository

---
+++ MVVM/Model/Repositories/LogRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalRegister.MVVM.Model.Repositories
+{
+    class LogRepository
+    {
+        public static void AddLog(Journal journal)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.SaveOrUpdate(journal); 
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}
