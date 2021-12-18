06f6bd944bc1fa2048aef42d4097285c1a18fc2a
Netyloft <netesov.9882@gmail.com>
2021-12-18 17:19:53 +0500

    добавил

---
+++ MVVM/Model/Mappers/ModelMappers/ScheduleCardModelMapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimalRegister.MVVM.Model.Mappers.ModelMappers
+{
+    public static class ScheduleCardModelMapper
+    {
+        public static ScheduleCardModel ToModel(ScheduleCard scheduleCard) =>
+            new ScheduleCardModel
+            {
+                Id = scheduleCard.Id,
+                Date = scheduleCard.Date,
+                Locality = scheduleCard.Locality,
+                District = scheduleCard.District,
+                Status = scheduleCard.Status
+            };
+
+        public static List<ScheduleCardModel> ToModel(IEnumerable<ScheduleCard> scheduleCards)
+        {
+            var res = new List<ScheduleCardModel>();
+            foreach (var scheduleCard in scheduleCards)
+            {
+                res.Add(ToModel(scheduleCard));
+            }
+
+            return res;
+            //return scheduleCards.Select(ToModel).ToList();
+        }
+    }
+}
---
+++ MVVM/Model/ScheduleCardModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AnimalRegister.MVVM.Model
+{
+    public class ScheduleCardModel
+    {
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public string District { get; set; }
+        public string Locality { get; set; }
+        public string Status { get; set; }
+    }
+}
